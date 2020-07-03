d5ee8aff09f7258d82a0b97b4163a463a4f1bd4e
wbwiltshire <wbwiltshire@hotmail.com>
2020-07-03 10:45:27 -0400

    Updated to DotNet Core 3 and VS 2019

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SQLite;
+using Microsoft.Data.Sqlite;
 using System.Data;
 
 namespace ReadSQLite3
 {
     class Program
     {
-        private static string connectionString = @"Data Source=.\Customer.db3; FailIfMissing=True";
+        private static string connectionString = @"Data Source=.\Customer.db3;Cache=Shared";
         private static string queryTables = "SELECT name " +
             "FROM sqlite_master " +
             "WHERE type = 'table' " + 
@@ -23,133 +23,102 @@ namespace ReadSQLite3
         static void Main(string[] args)
         {
             Console.WriteLine("List of tables:");
-            GetTables();
-            Console.WriteLine("\nContacts using DataAdapter:");
-            UseAdapter();
-            Console.WriteLine("\nContacts using DataReader:");
-            UseReader();
+            using (SqliteConnection conn = new SqliteConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    if (conn.State == ConnectionState.Open)
+                    {
+                        GetTables(conn);
+                        
+                        Console.WriteLine("\nContacts using DataAdapter:");
+                        UseAdapter(conn);
 
+                        Console.WriteLine("\nContacts using DataReader:");                        
+                        UseReader(conn);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Unable to open database: {connectionString} ");
+                    }
+
+                }
+                catch (SqliteException se)
+                {
+                    Console.WriteLine($"Exception: {se.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception: {ex.Message}");
+                }
+            }
             Console.WriteLine("\nPress <Enter> to continue....");
             Console.ReadLine();
         }
 
-        private static void GetTables()
+        private static void GetTables(SqliteConnection conn)
         {
-            try
+            using (SqliteCommand command = conn.CreateCommand())
             {
-                using (SQLiteConnection conn = new SQLiteConnection(connectionString))
-                {
-                    conn.Open();
-                    if (conn.State == ConnectionState.Open)
-                    {
-                        using (SQLiteCommand command = conn.CreateCommand())
-                        {
-                            command.CommandText = queryTables;
+                command.CommandText = queryTables;
 
-                            using (SQLiteDataReader reader = command.ExecuteReader())
-                            {
-                                if (reader.HasRows)
-                                {
-                                    while (reader.Read())
-                                    {
-                                        Console.WriteLine($"\tName: {reader.GetString(0)}");
-                                    }
-                                }
-                                else
-                                    Console.WriteLine("No tables in database!");
-                            }
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"\tName: {reader.GetString(0)}");
                         }
                     }
+                    else
+                        Console.WriteLine("No tables in database!");
                 }
-            }
-            catch (SQLiteException se)
-            {
-                Console.WriteLine($"Exception: {se.Message}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception: {ex.Message}");
-            }
-
-        }
-
-        private static void UseAdapter()
-        {
-            SQLiteDataAdapter sqlAdapter = null;
-            DataSet ds = null;
-            object []items = null;
-            int records = 0;
-
-            try
-            {
-                using (SQLiteConnection conn = new SQLiteConnection(connectionString))
-                {
-                    conn.Open();
-                    if (conn.State == ConnectionState.Open)
-                    {
-                        using (SQLiteCommand command = conn.CreateCommand())
-                        {
-                            command.CommandText = query;
-                            sqlAdapter = new SQLiteDataAdapter();
-                            sqlAdapter.SelectCommand = command;
-                            ds = new System.Data.DataSet();
-
-                            records = sqlAdapter.Fill(ds);
-
-                            foreach (DataRow row in ds.Tables[0].Rows)
-                            {
-                                items = row.ItemArray;
-                                Console.WriteLine($"\tId: {items[0]}, First Name: {items[1]}, Last Name: {items[2]}");
-                            }
-                        }
-                    }
-                }
-            }
-            catch (SQLiteException se)
-            {
-                Console.WriteLine($"Exception: {se.Message}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception: {ex.Message}");
             }
         }
 
-        private static void UseReader()
+        private static void UseAdapter(SqliteConnection conn)
         {
+            //    //SqliteDataAdapter sqlAdapter = null;
+            //    DataSet ds = null;
+            //    object []items = null;
+            //    int records = 0;
 
-            try
-            {
-                using (SQLiteConnection conn = new SQLiteConnection(connectionString))
-                {
-                    conn.Open();
-                    if (conn.State == ConnectionState.Open)
-                    {
-                        using (SQLiteCommand command = conn.CreateCommand())
-                        {
-                            command.CommandText = query;
+            //using (SqliteCommand command = conn.CreateCommand())
+            //{
+            //    command.CommandText = query;
+            //    sqlAdapter = new SqliteDataAdapter();
+            //    sqlAdapter.SelectCommand = command;
+            //    ds = new System.Data.DataSet();
 
-                            using (SQLiteDataReader reader = command.ExecuteReader())
-                            {
-                                while (reader.Read())
-                                {
-                                    Console.WriteLine($"\tId: {reader.GetInt32(0)}, First Name: {reader[1]}, Last Name: {reader[2]},");
-                                }
+            //    records = sqlAdapter.Fill(ds);
 
-                            }
-                        }
-                    }
-                }
-            }
-            catch (SQLiteException se)
-            {
-                Console.WriteLine($"Exception: {se.Message}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception: {ex.Message}");
-            }
+            //    foreach (DataRow row in ds.Tables[0].Rows)
+            //    {
+            //        items = row.ItemArray;
+            //        Console.WriteLine($"\tId: {items[0]}, First Name: {items[1]}, Last Name: {items[2]}");
+            //    }
+            //}
+            Console.WriteLine($"\tNot implmented yet (https://github.com/dotnet/efcore/issues/13838)");
         }
 
+        private static void UseReader(SqliteConnection conn)
+        {
+
+            using (SqliteCommand command = conn.CreateCommand())
+            {
+                command.CommandText = query;
+
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"\tId: {reader.GetInt32(0)}, First Name: {reader[1]}, Last Name: {reader[2]},");
+                    }
+
+                }
+            }
+        }
     }
 }
